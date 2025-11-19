0556e51f2e40489220f7459a5fe69bf357073ff5
Stefano <stefanosilvestri2d@gmail.com>
2025-11-18 21:37:38 -0300

    Primeiro Comit

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -10,6 +10,6 @@ interface RegisterPayload {
 }
 
 export const registerUser = async (userData: RegisterPayload) => {
-  const response = await axios.post('/', userData);
+  const response = await axios.post('/user', userData);
   return response.data;
 };
